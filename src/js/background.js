ebc2327e11f73f4882ef1450b44c77c0d53e7e52
fukayatsu <fukayatsu@gmail.com>
2014-01-12 12:59:52 +0900

    first commit

---
+++ src/js/background.js
@@ -0,0 +1,30 @@
+// chrome.browserAction.onClicked.addListener(function(tab){
+//   chrome.tabs.create({"url": "https://ucchusma.herokuapp.com/"});
+// });
+
+var setStatus = function() {
+  $.ajax({
+    url: "https://ucchusma.herokuapp.com/api/v1/rooms/1.json",
+    success: function(res) {
+      var imgPaths = {
+        "vacant":   "../img/unlock_48.png",
+        "occupied": "../img/lock_48.png",
+      };
+
+      var imgPath = imgPaths[res.status];
+      var dateStr = moment(res.created_at).format("YYYY-MM-DD HH:mm:ss");
+
+      chrome.browserAction.setIcon({path: imgPath});
+      chrome.browserAction.setTitle({title: 'checked at ' + dateStr});
+    },
+    error: function(xhr, status, error) {
+      chrome.browserAction.setIcon({path: imgPaths['vacant']});
+      chrome.browserAction.setTitle({title: "can't get status."});
+    }
+  });
+};
+
+$(function() {
+  setStatus();
+  setInterval(setStatus, 60 * 1000);
+});
